402e0b7fb192d2a9ddbe2dc0cdafd3ffea638ee2
David Fanchic Chatelard <davidfchatelard@gmail.com>
2022-04-12 16:37:35 -0300

    Codigos da thami com alterações
    mudamos os valores de algumas constantes para o nosso robo, porém o encoder não está lendo direito ainda. O caio acha que o problema é na função attachInterrupt

--- Codigos/CodigosTrekking2022/src/main.cpp
+++ Codigos/CodigosTrekking2022/src/main.cpp
@@ -2,68 +2,82 @@
 #include <MotorDC.h>
 #include <SensorColor.h>
 #include <Ultrasonic.h>
+#include <Move.h>
 
-// Motor Direita Frente
+// Motor Direita
 #define pin1A 5
-#define pin1B 6
-#define pin1pwm 7
-#define pin1Enc A0
+#define pin1B 7
+#define pin1pwm 8
+#define pin1Enc 18
 
-// Motor Direita Tras
-#define pin1A 5
-#define pin1B 6
-#define pin1pwm 7
-#define pin1Enc A0
-
-// Motor Esquerda Frente
-#define pin2A 8
-#define pin2B 9
-#define pin2pwm 10
-#define pin2Enc A1
-
-// Motor Esquerda Tras
-#define pin2A 8
-#define pin2B 9
-#define pin2pwm 10
-#define pin2Enc A1
+// Motor Esquerda
+#define pin2A 6
+#define pin2B 4
+#define pin2pwm 9
+#define pin2Enc 19
 
 //Ultrassom
-#define pinFrontTrigger 52
-#define pinFrontEcho 53
+// #define pinTrigger 52
+// #define pinEcho 53
 
 // Sensor de cor
-#define pinColorLeftS0 44
-#define pinColorLeftS1 45
-#define pinColorLeftS2 46
-#define pinColorLeftS3 47
-#define pinColorLeftOut 43
+// #define pinColorS0 44
+// #define pinColorS1 45
+// #define pinColorS2 46
+// #define pinColorS3 47
+// #define pinColorOut 43
 
 
 MotorDC motorRight(pin1A, pin1B, pin1pwm, pin1Enc);
 MotorDC motorLeft(pin2A, pin2B, pin2pwm, pin2Enc);
 
-Ultrasonic sensorUltra(pinFrontTrigger, pinFrontEcho);
+// Ultrasonic sensorUltra(pinTrigger, pinEcho);
 
-SensorColor sensorColor(pinColorLeftS0, pinColorLeftS1, pinColorLeftS2, pinColorLeftS3, pinColorLeftOut);
+// SensorColor sensorColor(pinColorS0, pinColorS1, pinColorS2, pinColorS3, pinColorOut);
+
+void inc (){
+
+  motorLeft.encSignal();
+  
+}
+
+void incR (){
+
+	motorRight.encSignal();
+  Serial.print("Enc 1: ");
+  Serial.println(motorRight.getCount());
+}
 
 
 void setup() {
   Serial.begin(9600);
-
+  attachInterrupt(digitalPinToInterrupt(pin1Enc), incR, RISING);
+  attachInterrupt(digitalPinToInterrupt(pin2Enc), inc, RISING);
+  motorRight.fwd(50);
+  motorLeft.fwd(50);
 }
 
 
 void loop() {
-  // motorRight.fwd(70);
-  // motorLeft.fwd(70);
-  // delay(5000);
+  // -------------------------------------------------------------------------------------------------------------
+  // Código de teste dos Motores
+  
+  Serial.print("Enc 2: ");
+  Serial.println(motorLeft.getCount());
+
+  // delay(2000);
   // motorRight.stop();
   // motorLeft.stop();
-  // delay(5000);
+  // delay(2000);
   // motorRight.rev(70);
   // motorLeft.rev(70);
-  // delay(5000);
+  // delay(2000);
+  // motorRight.stop();
+  // motorLeft.stop();
+  // delay(2000);
 
+  // -------------------------------------------------------------------------------------------------------------
+  // Código de teste para o Ultrassom
   //Le as informacoes do sensor, em cm e pol
   // float cmMsecFront, cmMsecLeft, cmMsecRight;
   // long microsecFront = sensorUltra.timing();
@@ -75,23 +89,26 @@ void loop() {
   // Serial.print("\n");
   // delay(1000);
 
-  sensorColor.readColors(); // Chama a rotina que le as cores
 
-  // Verifica se a cor vermelha foi detectada
-  if (sensorColor.vermelho < sensorColor.azul && sensorColor.vermelho < sensorColor.verde){
-    Serial.print("VERMELHO");
-    Serial.print("\n");
-  }
-  
-  if (sensorColor.azul < sensorColor.vermelho && sensorColor.azul < sensorColor.verde) { // Verifica se a cor azul foi detectada
-    Serial.print("AZUL");
-    Serial.print("\n");
-  }
-  
-  if (sensorColor.verde < sensorColor.vermelho && sensorColor.verde < sensorColor.azul) { // Verifica se a cor verde foi detectada
-    Serial.print("VERDE");
-    Serial.print("\n");
-  }
+  // -------------------------------------------------------------------------------------------------------------
+  // Código de teste para o sensor de Cor
+  // sensorColor.readColors(); // Chama a rotina que le as cores
 
-  delay(1000); // Aguarda 1000 milissegundos
+  // // Verifica se a cor vermelha foi detectada
+  // if (sensorColor.vermelho < sensorColor.azul && sensorColor.vermelho < sensorColor.verde){
+  //   Serial.print("VERMELHO");
+  //   Serial.print("\n");
+  // }
+  
+  // if (sensorColor.azul < sensorColor.vermelho && sensorColor.azul < sensorColor.verde) { // Verifica se a cor azul foi detectada
+  //   Serial.print("AZUL");
+  //   Serial.print("\n");
+  // }
+  
+  // if (sensorColor.verde < sensorColor.vermelho && sensorColor.verde < sensorColor.azul) { // Verifica se a cor verde foi detectada
+  //   Serial.print("VERDE");
+  //   Serial.print("\n");
+  // }
+
+  //delay(1000); // Aguarda 1000 milissegundos
 }
