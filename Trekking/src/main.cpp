9cae2f43e46de1b2e91c7efc559ddd16886c2522
Lucas <13675790+lschiavini@users.noreply.github.com>
2022-08-31 20:29:55 -0300

    Fixing merge issues

--- Trekking/src/main.cpp
+++ Trekking/src/main.cpp
@@ -7,44 +7,7 @@
 #include <Wire.h>
 #include <ColorSensor.h>
 #include <string.h>
-
-// Motor Direita
-#define pin1A 3
-#define pin1B 7
-#define pin1pwm 8
-#define pin1Enc A0
-
-// Motor Esquerda
-#define pin2A 6
-#define pin2B 2
-#define pin2pwm 9
-#define pin2Enc A1
-
-#define pinEnable1 14
-#define pinEnable2 15
-
-//Ultrassom Frente
-#define pinFrontTrigger 52
-#define pinFrontEcho 53
-
-//Ultrassom Esquerda
-#define pinLeftTrigger 50
-#define pinLeftEcho 51
-
-//Ultrassom Direita
-#define pinRightTrigger 48
-#define pinRightEcho 49
-
-// Sensor de cor
-#define pinColorS0 4 // A DEFINIR
-#define pinColorS1 5 // A DEFINIR
-#define pinColorS2 6 // A DEFINIR
-#define pinColorS3 7 // A DEFINIR
-#define pinColorOut 8 // A DEFINIR
-
-#define EIXO_X 0
-#define EIXO_Y 1
-#define EIXO_Z 2
+#include <defines.h>
 
 int firstReading = true;
 int count = 0;
@@ -120,6 +83,7 @@ void gyroTest() {
   // -----------------------------------------------------------------
 }
 
+void commentedTest() {
   // -----------------------------------------------------------------
   // TESTE SENSOR DE COR
   // colorSensor->readColor();
