716ddd54262b257fd2b93b48ac19f09ac2401fb5
Lucas <13675790+lschiavini@users.noreply.github.com>
2022-10-23 18:48:03 -0300

    Comunicaçao 7 bits feiona

--- Trekking/test/test_rasp_arduino_comm.cpp
+++ Trekking/test/test_rasp_arduino_comm.cpp
@@ -1,4 +1,3 @@
-
 #include <Arduino.h>
 #include <Move.h>
 #include <Gyro.h>
@@ -18,12 +17,22 @@ void setup() {
 
 
 void loop() {
-    uint8_t bits[4];
+    uint8_t bits[7];
     bits[0] = digitalRead(14);
     bits[1] = digitalRead(15);
     bits[2] = digitalRead(16);
     bits[3] = digitalRead(17);
-    int decValue = bits[0] + (bits[1] * 2) + (bits[2]*2*2) + (bits[3]*2*2*2); 
+    bits[4] = digitalRead(26);
+    bits[5] = digitalRead(28);
+    bits[6] = digitalRead(34);
+
+    int decValue = bits[0] + 
+        (bits[1]*2) + 
+        (bits[2]*2*2) + 
+        (bits[3]*2*2*2) + 
+        (bits[4]*2*2*2*2) + 
+        (bits[5]*2*2*2*2*2) + 
+        (bits[6]*2*2*2*2*2*2);
 
     Serial.print("Angulo do Cone = ");
     Serial.println(decValue);
