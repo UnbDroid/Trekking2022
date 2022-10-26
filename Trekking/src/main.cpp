ab1e3099cfe666318b34b36af388c6fac86fc3e9
Lucas <13675790+lschiavini@users.noreply.github.com>
2022-10-25 21:37:34 -0300

    Anda ate achar cone us

--- Trekking/src/main.cpp
+++ Trekking/src/main.cpp
@@ -1,42 +1,60 @@
 #include <Arduino.h>
+#include <MotorDC.h>
+#include <ColorSensor.h>
+#include <Ultrasonic.h>
 #include <Move.h>
 #include <Gyro.h>
 #include <Wire.h>
-#include <string.h>
 #include <defines.h>
 
-Gyro *giroscopio = new Gyro();
+Ultrasonic ultrasonic1(US1Trigger, US1Echo);
+Ultrasonic ultrasonic2(US2Trigger, US2Echo);
+Ultrasonic ultrasonic3(US3Trigger, US3Echo);
+Ultrasonic ultrasonic4(US4Trigger, US4Echo);
 
-void setup() {
+MotorDC motorRight (pin1A, pin1B, pin1pwm, pin1Enc, pinEnable1); 
+MotorDC motorLeft (pin2A, pin2B, pin2pwm, pin2Enc, pinEnable2);
+void setup()
+{
     Serial.begin(9600);
-    Wire.begin();
-    
-    // Inicializa o HMC5883
-    Wire.beginTransmission(address);
-    // Seleciona o modo
-    Wire.write(0x02); 
-    // Modo de medicao continuo
-    Wire.write(0x00); 
-    Wire.endTransmission();
-    pinMode(SinalLuminoso, OUTPUT);
-    digitalWrite(SinalLuminoso, LOW);
 }
 
-void gyroTest() {
-  double gyroValue = giroscopio->requestData();
-  // -----------------------------------------------------------------
-  Serial.print("gyroValue: ");
-  Serial.println(gyroValue);
-  // -----------------------------------------------------------------
-}
-
-void loop() {
-    digitalWrite(SinalLuminoso, HIGH);
-    delay(500);
-    digitalWrite(SinalLuminoso, LOW);
-    delay(500);
+void loop()
+{
+    // Le as informacoes do sensor, em cm e pol
+    float cm2Msec;
+    float cm3Msec;
+    float cm4Msec;
 
     
-    gyroTest();
-    gyroTest();
+    long microsec2 = ultrasonic2.timing();
+    cm2Msec = ultrasonic2.convert(microsec2, Ultrasonic::CM);
+ 
+    long microsec3 = ultrasonic3.timing();
+    cm3Msec = ultrasonic3.convert(microsec3, Ultrasonic::CM);
+ 
+    long microsec4 = ultrasonic4.timing();
+    cm4Msec = ultrasonic4.convert(microsec4, Ultrasonic::CM);
+ 
+    
+    Serial.print("\tUS2 cm: ");
+    Serial.print(cm2Msec);
+    
+    Serial.print("\tUS3 cm: ");
+    Serial.print(cm3Msec);
+    
+    Serial.print("\tUS4 cm: ");
+    Serial.print(cm4Msec);
+    
+    Serial.println(); 
+    if(cm3Msec < 40 || cm2Msec < 40 ||cm4Msec < 40 ) {
+      // moveAll(40, &motorLeft, &motorRight);
+      // delay(100);
+      stopAll(&motorLeft, &motorRight);
+    } else {
+      moveRevAll(40, &motorLeft, &motorRight);
+    }
+    
+
+    delay(500);
 }
