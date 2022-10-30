4c15de558805f1d24157982d8a28ca729cd74032
Lucas <13675790+lschiavini@users.noreply.github.com>
2022-10-30 18:09:10 -0300

    Comm arduino

--- Trekking/test/test_rasp_arduino_comm.cpp
+++ Trekking/test/test_rasp_arduino_comm.cpp
@@ -1,42 +1,35 @@
 #include <Arduino.h>
+#include <MotorDC.h>
+#include <ColorSensor.h>
+#include <Ultrasonic.h>
 #include <Move.h>
 #include <Gyro.h>
 #include <Wire.h>
-#include <string.h>
 #include <defines.h>
 
-Gyro *giroscopio = new Gyro();
-
-void setup() {
+void setup()
+{
     Serial.begin(9600);
-    pinMode(14, INPUT);
-    pinMode(15, INPUT);
-    pinMode(16, INPUT);
-    pinMode(17, INPUT);
+    pinMode(27, INPUT);
+    pinMode(29, INPUT);
+    pinMode(31, INPUT);
+    pinMode(33, INPUT);
+    pinMode(35, INPUT);
+
+    pinMode(37, INPUT);
+    pinMode(39, INPUT);
 }
 
-
-void loop() {
-    uint8_t bits[7];
-    bits[0] = digitalRead(14);
-    bits[1] = digitalRead(15);
-    bits[2] = digitalRead(16);
-    bits[3] = digitalRead(17);
-    bits[4] = digitalRead(26);
-    bits[5] = digitalRead(28);
-    bits[6] = digitalRead(34);
-
-    int decValue = bits[0] + 
-        (bits[1]*2) + 
-        (bits[2]*2*2) + 
-        (bits[3]*2*2*2) + 
-        (bits[4]*2*2*2*2) + 
-        (bits[5]*2*2*2*2*2) + 
-        (bits[6]*2*2*2*2*2*2);
-
-    Serial.print("Angulo do Cone = ");
-    Serial.println(decValue);
+void loop()
+{
+    uint8_t sumBits = 0;
+    uint8_t exp = 0;
 
 
+    for(int i = 27; i < 40; i+=2) {
+        sumBits += digitalRead(i) << exp;
+        exp++;
+    }
 
+    Serial.println(sumBits);
 }
